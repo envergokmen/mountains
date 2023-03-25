d9b33885886bce34a7d20a936d545106cadb2d99
Enver GÖKMEN <envergokmen@gmail.com>
2023-03-25 19:48:48 +0300

    range slider added mountain list added

--- js/mountain.js
+++ js/mountain.js
@@ -1,17 +1,8 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
    
-    // const request = window.indexedDB.open('MOUNTAINS_DB', 1);
-
-    // request.onerror = (event) => {
-    //     // Do something with request.errorCode!
-    //   };
-    //   request.onsuccess = (event) => {
-    //     // Do something with request.result!
-    //   };
-
-    //  window.indexedDB.deleteDatabase('MOUNTAINS_DB', 1);
-
+     $mountItemTemplate=$(".mount-item-template").first().clone();
+     $MountList=$("#MountList");
 
     // 1
     const indexedDB =
@@ -30,8 +21,9 @@ $( document ).ready(function() {
 
     console.log({mountain_data_upto_date_ix});
 
-    if(!mountain_data_upto_date_ix)
+    if(!mountain_data_upto_date_ix || mountain_data_upto_date_ix=='false')
     {
+        mountain_data_upto_date_ix=false;
         deleteRequest = indexedDB.deleteDatabase("MOUNTAINS_DB");
     }
     
@@ -45,35 +37,28 @@ $( document ).ready(function() {
      var db=null;
 
      request.onupgradeneeded = function () {
-        //1
+       
          db = request.result;
-      
-        //2
         const store = db.createObjectStore("Mountains", { keyPath: "id",autoIncrement:true });
       
-        //3
-        store.createIndex("Lat_IX", ["lat"], { unique: false });
-        store.createIndex("Long_IX", ["long"], { unique: false });
-        store.createIndex("Alt_IX", ["alt"], { unique: false });
-        store.createIndex("Cont_IX", ["cont"], { unique: false });
-        store.createIndex("Name_IX", ["name"], { unique: true });
- 
-        // 4
+        store.createIndex("Lat_IX", "lat", { unique: false });
+        store.createIndex("Long_IX", "long", { unique: false });
+        store.createIndex("Alt_IX", "alt", { unique: false });
+        store.createIndex("Cont_IX", "cont", { unique: false });
+        store.createIndex("Name_IX", "name", { unique: true });
         store.createIndex("Long_and_Lat_IX", ["long", "lat"], {
           unique: false,
         }); 
+
     };
 
 
     request.onsuccess = function () {
-    //     console.log("Database opened successfully");
       
         const db = request.result;
-    //// 1
         const transaction = db.transaction("Mountains", "readwrite");
-      
-    //     //2
-         const store = transaction.objectStore("Mountains");
+        const store = transaction.objectStore("Mountains");
+
     //     const altIndex = store.index("Alt_IX");
     //     const makeModelIndex = store.index("Long_and_Lat_IX");
 
@@ -84,14 +69,24 @@ $( document ).ready(function() {
         // store.put({ id: 4, colour: "Silver", make: "Subaru" });
         
         // //4
-        // const idQuery = store.get(4);
+         const firstItemQuery = store.get(1);
         // const colourQuery = colourIndex.getAll(["Red"]);
         // const colourMakeQuery = makeModelIndex.get(["Blue", "Honda"]);
       
         // // 5
-        // idQuery.onsuccess = function () {
-        //   console.log('idQuery', idQuery.result);
-        // };
+        firstItemQuery.onsuccess = function () {
+
+            if(!(firstItemQuery.result))
+            {
+                mountain_data_upto_date_ix=false;
+                sessionStorage.setItem("mountain_data_upto_date_ix", false);
+                
+                InitMountainData();
+            }
+
+          //console.log('idQuery', firstItemQuery.result);
+        };
+
         // colourQuery.onsuccess = function () {
         //   console.log('colourQuery', colourQuery.result);
         // };
@@ -109,87 +104,132 @@ $( document ).ready(function() {
 
       if(!mountain_data_upto_date_ix)
       {
-          $.ajax({
-              type: "GET",
-              url: '/js/mountains.json',
-              dataType: "json",
-              jsonp: true,
-              success: function(mounts) {
-                 
-                const request2 = indexedDB.open("MOUNTAINS_DB", 1);
-                
-                request2.onsuccess = function () {
-
-                    var db2 = request2.result;
-                   
-                    const transaction2 = db2.transaction("Mountains", "readwrite");
-                    const store2 = transaction2.objectStore("Mountains");
-                    var _id=0;
-
-                    mounts.forEach(mount=>  {
-                        store2.put({ name: mount.name, long: mount.long, lat: mount.lat, alt:mount.alt, cont: mount.cont});  
-                        _id++;
-                      });
-                    }
-
-                    request2.oncomplete = function () {
-                        db2.close();
-                 };
-              
-                  sessionStorage.setItem("mountain_data_upto_date_ix",true);
-              }
-          });
+          InitMountainData();
       }
+    
+
+      
+    function InitMountainData() {
+        $.ajax({
+            type: "GET",
+            url: '/js/mountains.json',
+            dataType: "json",
+            jsonp: true,
+            success: function (mounts) {
+
+                const dbOpenRequest = indexedDB.open("MOUNTAINS_DB", 1);
+                dbOpenRequest.onsuccess = function () {
+
+                    var database = dbOpenRequest.result;
+
+                    const tran = database.transaction("Mountains", "readwrite");
+                    const mountains = tran.objectStore("Mountains");
+                    var _id = 0;
+
+                    mounts.forEach(mount => {
+                        console.log({ name: mount.name, long: mount.long, lat: mount.lat, alt: mount.alt, cont: mount.cont });
+                        mountains.put({ name: mount.name, long: mount.long, lat: mount.lat, alt: mount.alt, cont: mount.cont });
+                        _id++;
+                    });
+                };
+
+                dbOpenRequest.oncomplete = function () {
+                    database.close();
+                };
+
+                sessionStorage.setItem("mountain_data_upto_date_ix", true);
+            }
+        });
+    }
 
 
-
-/*
-    var db = openDatabase('MOUNTAINS_DB', '1.0', 'Mountains Database', 2 * 1024 * 1024);  
-
-    var totalMountain=0;
-
-    var mountain_data_upto_date = sessionStorage.getItem("mountain_data_initilized");
-
-    db.transaction(function (tx) { 
-        
-        if(!mountain_data_upto_date)
-        {
-            tx.executeSql('DROP TABLE Mountains');  
+    $( "#slider" ).slider({
+        range: true,
+       
+        min: 500,
+        max: 9000,
+        step: 100,
+        values: [ 8000, 8890 ],
+        slide: function( event, ui ) {
+            $("#altitude-input").val( ui.values[0] + " - " + ui.values[1] +" Meters" );
         }
+      });
+
+      $( "#altitude-input" ).val( $( "#slider" ).slider( "values", 0 ) +
+      " - " + $( "#slider" ).slider( "values", 1 )+" Meters" );
+
+              
+      $( ".main-header" ).text("Find Mountains in "+ $( "#slider" ).slider( "values", 0 ) + " - "+
+       $( "#slider" ).slider( "values", 1 )+" Meters..." );
+
+      setTimeout(() => {
+
+
+        LoadMountainData(Number($( "#slider" ).slider( "values", 0 )), Number($( "#slider" ).slider( "values", 1 )))
         
-        tx.executeSql('CREATE TABLE IF NOT EXISTS Mountains (name,long,lat,alt,cont)'); 
-    });  
+      }, 500);
 
-    db.transaction(function (tx) { 
-    tx.executeSql('SELECT count(*) as TotalCount FROM Mountains', [], function (tx, results) { 
-        totalMountain = Number(results.rows.item(0).TotalCount);
-        console.log({totalMountain});
+      $( "#slider" ).on( "slidechange", function( event, ui ) {
 
-        if(totalMountain==0)
-        {
-            $.ajax({
-                type: "GET",
-                url: '/js/mountains.json',
-                dataType: "json",
-                jsonp: true,
-                success: function(mounts) {
-                   
-    
-                    db.transaction(function (tx) { 
-    
-                        mounts.forEach(mount=>  {
-                            tx.executeSql('INSERT INTO Mountains (name,long,lat,alt,cont) VALUES ("'+mount.name+'",'+mount.long+','+mount.lat+','+ mount.alt+',"'+mount.cont+'")');   
-                        });
-                       
-                    }); 
-                    
-                    sessionStorage.setItem("mountain_data_initilized",true);
-                }
-            });
-        }
+        $( "#altitude-input" ).val( $( "#slider" ).slider( "values", 0 ) +
+        " - " + $( "#slider" ).slider( "values", 1 )+" Meters..." );
+        
+        $( ".main-header" ).text("Find Mountains in "+ $( "#slider" ).slider( "values", 0 ) + " - "+
+        $( "#slider" ).slider( "values", 1 )+" Meters..." );
+ 
+        LoadMountainData(Number($( "#slider" ).slider( "values", 0 )), Number($( "#slider" ).slider( "values", 1 )))
 
-    }, null); 
-    });
-*/
+      });
+
+
+        function LoadMountainData(altitudeStarts, altitudeEnds, continent) {
+
+                console.log("loading mountain data for : ", {altitudeStarts}, {altitudeEnds}, {continent});
+
+                const dbOpenRequest = indexedDB.open("MOUNTAINS_DB", 1);
+                dbOpenRequest.onsuccess = function () {
+
+                    var database = dbOpenRequest.result; 
+                    const tran = database.transaction("Mountains", "readwrite");
+
+                    const mountains = tran.objectStore("Mountains"); 
+                    const range = IDBKeyRange.bound(altitudeStarts, altitudeEnds);
+                    const query = mountains.index('Alt_IX').openCursor(range, 'prev');
+                
+                    $("#MountList tbody").remove();
+
+                // Iterate over the results and log them to the console
+                query.onsuccess = (event) => {
+                    const cursor = event.target.result;
+                     
+                  // console.log(cursor.key, cursor.value);
+                        
+                    if (cursor) {
+                            
+                            var data = cursor.value;
+
+                            $tempItem = $mountItemTemplate.clone();
+
+                            $tempItem.find(".name").text(data.name);
+                            $tempItem.find(".altitude").text(data.alt);
+                            $tempItem.find(".coords").text(data.lat+"-"+data.long);
+                            $tempItem.find(".countinent").text(data.cont);
+                            
+                            if((continent==null || continent==undefined) ||  data.cont == continent)
+                            {
+                                $("#MountList").append($tempItem);
+                            }
+
+                            cursor.continue();
+                   }
+            }
+
+            dbOpenRequest.oncomplete = function () {
+                database.close();
+            };
+
+
+      }}
     
+
 });
